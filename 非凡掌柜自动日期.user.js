ea7becbb9db4127317cb2b1109096895099b0b93
Kung <sales@caryelectronic.com>
2018-11-27 22:26:14 +0800

    create function fillZero()

--- 非凡掌柜自动日期.user.js
+++ 非凡掌柜自动日期.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         非凡掌柜自动日期
 // @namespace    https://github.com/carycoti/myGM
-// @version      1.2.0
+// @version      1.3.0
 // @description  非凡掌柜自动填写开始和结束日期，默认7天
 // @author       Kung
 // @match        *://shop.zhuofannuo.com/discountMod*
@@ -11,8 +11,16 @@
 function fun_date(aa, sep="-"){
         let date = new Date();
         date.setDate(date.getDate()+aa);
-    return date.getFullYear()+sep+(date.getMonth()+1)+sep+date.getDate();
+    return date.getFullYear()+sep+(fillZero(date.getMonth()+1))+sep+fillZero(date.getDate());
     }
+
+
+function fillZero(value){
+if(value.toString().length<2){
+    return "0"+value;
+}
+return value;
+}
 
 const start = fun_date(0) + " 22:00:00";
 const start_date = document.getElementById("startTime");
