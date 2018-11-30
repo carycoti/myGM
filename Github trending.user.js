237f96f59ae01ce6823e46e3cfcafbb4d04cb2ba
Kung Liao <sales@caryelectronic.com>
2018-11-30 16:37:57 +0800

    1.0.1

--- Github trending.user.js
+++ Github trending.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Github Trending
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.0.1
 // @description  添加Trending链接到Github首页
 // @author       Kung
 // @match        *://github.com/*
@@ -15,8 +15,9 @@ function main(){
     let my_ul = document.querySelectorAll("ul.list-style-none")[0];
     let my_trending = document.createElement("li");
     let my_a = document.createElement("a");
-    my_a.href = "https://github.com/trending/python?since=daily";
+    my_a.href = "/trending/python?since=daily";
     my_a.id = "my-trending-url";
+    my_a.className = "js-selected-navigation-item HeaderNavlink px-lg-2 py-2 py-lg-0";
     my_a.textContent = "Trending";
     my_trending.appendChild(my_a);
     let my_li = my_ul.querySelectorAll("li");
