e208ab5db7e4d331865c23dd6987cb722aee9563
Kung <sales@caryelectronic.com>
2018-12-04 20:58:27 +0800

    some

--- 豆瓣 ReadFree 传送门.user.js
+++ 豆瓣 ReadFree 传送门.user.js
@@ -15,7 +15,7 @@
 const log = console.log.bind(console);
 
 function get_book_id(url) {
-    let re = /^\/subject\/(\d+)/g;
+    let re = /\/subject\/(\d+?)/g;
     let matches = re.exec(url);
     if (matches && matches.length > 1) {
         return parseInt(matches[1]);
@@ -39,52 +39,46 @@ function dom(tag, attr, inner) {
     return el;
 }
 
-function get_status(url) {
-    let id = get_book_id(url);
+function rf_book_body() {
+    let path = document.location.pathname;
+    let id = get_book_id(path);
     if (id) {
         let rf_url = 'http://readfree.me/book/' + id;
         GM_xmlhttpRequest({
             method: "HEAD",
             url: rf_url,
             onload: function (response) {
-                return response.status;
+                if (response.status === 200) {
+                    let panel = dom('div', {id: 'readfree-link'});
+                    let ahref = dom('a', {href: rf_url, target: '_blank'}, 'ReadFree!');
+                    panel.appendChild(ahref);
+                    document.body.appendChild(panel);
+                    add_readfree_style();
+                }
             }
         })
     }
 }
 
-function rf_book_body() {
-    let path = document.location.pathname;
-    log(path);
-    let status = get_status(path);
-    if (status === 200) {
-        let panel = dom('div', { id : 'readfree-link' });
-        let ahref = dom('a', { href : rf_url, target : '_blank' }, 'ReadFree!');
-        panel.appendChild(ahref);
-        document.body.appendChild(panel);
-        add_readfree_style();
-    }
-}
-
-function all_book() {
-    let my_a = document.querySelectorAll("a");
-    for (let i = 0; i < my_a.length; i++) {
-        let this_a = my_a[i];
-        if (this_a.href.indexOf("subject")){
-            let status = get_status(this_a.href);
-            if (status === 200) {
-                let panel = dom('div', { id : 'readfree-link' });
-                let ahref = dom('a', { href : rf_url, target : '_blank' }, 'ReadFree!');
-                panel.appendChild(ahref);
-                this_a.parentNode.insertBefore(panel, this_a.nextSibling);
-            }
-        }
-    }
-}
+// function all_book() {
+//     let my_a = document.querySelectorAll("a");
+//     for (let i = 0; i < my_a.length; i++) {
+//         let this_a = my_a[i];
+//         if (this_a.href.indexOf("subject")){
+//             let status = get_status(this_a.href);
+//             if (status === 200) {
+//                 let panel = dom('div', { id : 'readfree-link' });
+//                 let ahref = dom('a', { href : rf_url, target : '_blank' }, 'ReadFree!');
+//                 panel.appendChild(ahref);
+//                 this_a.parentNode.insertBefore(panel, this_a.nextSibling);
+//             }
+//         }
+//     }
+// }
 
 function main(){
     rf_book_body();
-    all_book();
+    // all_book();
 }
 
 log(window.setTimeout(function(){main();}, 0));
