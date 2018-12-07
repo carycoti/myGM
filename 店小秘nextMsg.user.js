8606f428364ed7013ee02c57f535d3575ed8f38b
Kung <sales@caryelectronic.com>
2018-12-07 23:26:41 +0800

    cookies

--- 店小秘nextMsg.user.js
+++ 店小秘nextMsg.user.js
@@ -35,10 +35,14 @@ function add_history_orders() {
     GM_xmlhttpRequest({
         method: "HEAD",
         url: order_url,
+        headers:{
+            userAgent: 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
+            cookie: '_ati=4589820662837; dxm_i=NzE4NTghYVQwM01UZzFPQSFhNWQzZmNiYWQ2ZTA5YzRmYzAxYWIyNWM2NmNkYTRhMA; dxm_t=MTU0MDIxMzU0MSFkRDB4TlRRd01qRXpOVFF4IWEyNDk1MDljMDk4NzY2MTYwMzFiNGQ3NTU1YmE0M2Vj; dxm_c=YWgzTVh0MUQhWXoxaGFETk5XSFF4UkEhMzliYWVkOWMwMzk1YzdlNTVmNWQzMjYxZTNiNTM3ZDE; dxm_w=OThjOThmYmE1OGM5MmQxZDU1YjM5OTgxZDk1ZTczMGIhZHowNU9HTTVPR1ppWVRVNFl6a3laREZrTlRWaU16azVPREZrT1RWbE56TXdZZyExMmRhOThhNTdhM2IzYjNkZjIyMTVlNGRmNGI0NmZlNw; dxm_s=E37unnxP8VD8gD3LzSKGvPZF2pStjgMkqZOp7VPCm1A; Hm_lvt_f8001a3f3d9bf5923f780580eb550c0b=1543734432; JSESSIONID=EE7E39B42182B9E101C607F3384681CD; rememberMe=yDD1Gb23z9V2U1a2IFuMym94XtEN3k/oVkWJ6tE2w7U4MKMu/EP6agb9K7lnQ66UKlsVpqCeUUf+aymOpSnCKnWbBbwO6FqmnQ3XjeCwcl3RbeExylZ2NcwyjfaiSOgwtULILnrdMAcgcC9NTebLI6pKBgVMlxY6BYQjh7NTNZkbLUj4NwHrBRQPVZrNi8q8Zi+/VI1Hu+ZYwRtfMvaQCtHC5xcrRCTpPCI8WTYvq6np0NWiNlvFh/v4H0LnNxJcUzWw2eozpqvDEdqEwEbHAHcOxuZEgoGem+3Ee/ohk58eFxcafRsyETTJrb7vBc9r3BeSsFMWha8Y1t9L2bX+jpKWt6oYlrOm1Wd3KXirIdo/WwZIP8jcJaiYcZKc2WOW9EBceC/r32QfHrP/+zsQe5fmSWLUxdYP+AoQMIUL87PMQEaujeE31C5GCGsIZw7SOIrOyB7OJhy3sjGdlbcbv8FOv5+xOf1tmKsB8RVbhabQnN79blThNXnmgRD3yar859lI95P3XyC8OxeH+gTFzgKWIrG4in8ok0HXUSpFDDw=; Hm_lpvt_f8001a3f3d9bf5923f780580eb550c0b=1544195148',
+        },
         onload: function (response) {
             log(response.responseText)
         }
-    });
+    })
 }
 
 function main() {
