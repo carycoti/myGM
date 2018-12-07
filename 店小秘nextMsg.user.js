99ded18be8dc2a3a030df92c55aa34c70b65e5ce
Kung <sales@caryelectronic.com>
2018-12-07 23:37:46 +0800

    1.3.0

--- 店小秘nextMsg.user.js
+++ 店小秘nextMsg.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         店小秘nextMsg
 // @namespace    https://github.com/carycoti/myGM
-// @version      1.2.0
+// @version      1.3.0
 // @description  店小秘自动勾选nextMsg：提交发送后，自动打开下一封消息
 // @author       Kung
 // @match        *://www.dianxiaomi.com/ebayMessage/*
