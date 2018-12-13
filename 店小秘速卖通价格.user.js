1526ebd6de35a48f4f6650d185f37c11e8265a01
Kung <sales@caryelectronic.com>
2018-12-14 03:04:05 +0800

    1.1.1

--- 店小秘速卖通价格.user.js
+++ 店小秘速卖通价格.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         店小秘速卖通价格
 // @namespace    http://tampermonkey.net/
-// @version      1.1.0
+// @version      1.1.1
 // @description  店小秘速卖通产品的价格
 // @author       Kung
 // @match        *://www.dianxiaomi.com/smtProduct/batchEdit.htm
@@ -10,16 +10,22 @@
 // ==/UserScript==
 
 function main() {
-    let re = /.+(\d)+.+?/g;
+    let re = /.+(\d+).+?/g;
     let this_div = document.querySelectorAll('div.typeDiv');
     let num = this_div.length / 12;
     let result = [];
     for (let i = 0; i < num; i++) {
         let sku = this_div[6 + 12 * i].querySelector('input').value;
         let title = this_div[12 * i].querySelector('textarea').textContent;
-        let group = this_div[1 + 12 * i].querySelector('a[@name="real"]').textContent.trim();
-        let qty = this_div[2 + 12 * i].querySelector('a[@name="real"]').textContent.trim();
-        qty = re.exec(qty)[1];
+        let group = this_div[1 + 12 * i].querySelector('a[name=real]').textContent.trim();
+        let qty = this_div[2 + 12 * i].querySelector('a[name=real]').textContent.trim();
+        matches = re.exec(qty);
+        if (matches && matches.length > 1) {
+            qty = parseInt(matches[1]);
+        }
+        else {
+            qty = 1;
+        }
         let price = this_div[3 + 12 * i].querySelector('input').value;
         let weight = this_div[8 + 12 * i].querySelector('input').value;
         let str = [];
