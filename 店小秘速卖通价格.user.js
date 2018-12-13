74b92b4960f0a8248eb89b6b55e697f440f0c769
Kung Liao <sales@caryelectronic.com>
2018-12-13 17:28:01 +0800

    update

--- 店小秘速卖通价格.user.js
+++ 店小秘速卖通价格.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  店小秘速卖通产品的价格
 // @author       Kung
-// @match        *://www.dianxiaomi.com/smtProduct/*
+// @match        *://www.dianxiaomi.com/smtProduct/batchEdit.htm
 // @grant        GM_setClipboard
 // @grant        GM_notification
 // ==/UserScript==
@@ -12,7 +12,7 @@
 const log = console.log.bind(console);
 
 function main() {
-    let this_div = document.querySelectorAll('tr td div[style="display:inline-block;"]');
+    let this_div = document.querySelectorAll('div.typeDiv');
     log(this_div)
 }
 
