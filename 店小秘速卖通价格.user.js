35b1846cba50917bba2824e67f8d00c9bbb40b19
Kung Liao <sales@caryelectronic.com>
2018-12-13 18:38:49 +0800

    1.0.0

--- 店小秘速卖通价格.user.js
+++ 店小秘速卖通价格.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         店小秘速卖通价格
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.0.0
 // @description  店小秘速卖通产品的价格
 // @author       Kung
 // @match        *://www.dianxiaomi.com/smtProduct/batchEdit.htm
@@ -13,7 +13,22 @@ const log = console.log.bind(console);
 
 function main() {
     let this_div = document.querySelectorAll('div.typeDiv');
-    log(this_div)
+    let num = this_div.length / 12;
+    let result = [];
+    for (let i = 0; i < num; i++) {
+        let sku_input = this_div[6 + 12 * i].querySelector('input');
+        let sku = sku_input.value;
+        let price_input = this_div[3 + 12 * i].querySelector('input');
+        let price = price_input.value;
+        let str = [];
+        str.push(sku);
+        str.push(price);
+        let str1 = str.join("\t");
+        result.push(str1);
+    }
+    let rst = result.join("\n");
+    GM_setClipboard(rst);
+    GM_notification("SKU & 价格已复制到剪切板中~");
 }
 
 log(window.setTimeout(function(){main();}, 0));
