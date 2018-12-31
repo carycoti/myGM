6b5d14b9726894d778521b8c6ad78dd36a750ada
Kung Liao <sales@caryelectronic.com>
2018-12-31 10:50:23 +0800

    some

--- 店小秘客服+++.user.js
+++ 店小秘客服+++.user.js
@@ -37,25 +37,23 @@ function add_history_orders() {
         this_div.replaceChild(my_a, this_span);
     }
     catch (e) {
-        console.log(e);
     }
 }
 
 function add_smt_history_orders() {
-        try {
+    try {
         let this_div = document.querySelector("div.gray-b");
         let this_span = this_div.querySelector("span.blacklist-icons");
         let customer = this_div.querySelector("span.f-black").textContent;
         customer = customer.replace(" ", "%20");
-        console.log(customer);
         let order_url = 'https://www.dianxiaomi.com/package/searchPackage.htm?pageNo=0&pageSize=100&state=shipped&shopId=-1&history=&searchType=contactName&content=' + customer + '&isVoided=0&isRemoved=0&commitPlatform=success&platform=&isGreen=0&isYellow=0&isOrange=0&isRed=0&isViolet=0&isBlue=0&cornflowerBlue=0&pink=0&teal=0&turquoise=0&isSearch=1&isFree=-1&isBatch=-1&isOversea=-1&orderField=shipped_time';
         let my_a = dom('a', {id: 'add_smt_history_orders', href: order_url, target: '_blank'}, '&nbsp; 历史订单>>>');
         this_div.replaceChild(my_a, this_span);
     }
     catch (e) {
-        console.log(e);
     }
 }
+
 function main() {
     checked();
     add_history_orders();
