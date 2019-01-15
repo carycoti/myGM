b22fac70a1445c0bd3e3684f7942be67af88fff1
Kung Liao <sales@caryelectronic.com>
2019-01-15 10:39:26 +0800

    订单SKU

---
+++ 网站后台生成订单sku.user.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name         网站后台生成订单sku
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  网站后台生成订单sku
+// @author       Kung
+// @match        https://www.caryelectronic.com/538xd2oda/index.php?controller=AdminOrders&id_order*
+// @grant GM_setClipboard
+// @grant GM_notification
+// ==/UserScript==
+
+const log = console.log.bind(console);
+
+function get_sku(text) {
+    let re = /Reference number (.+)"/g;
+    let matches = re.exec(text);
+    if (matches && matches.length > 1) {
+        return matches[1];
+    }
+}
+
+function dom(tag, attr, inner) {
+    let el = document.createElement(tag);
+    for (let key in attr) {
+        if (attr.hasOwnProperty(key)) {
+            el.setAttribute(key,attr[key]);
+        }
+    }
+    if (inner) {
+        el.innerHTML = inner;
+    }
+    return el;
+}
+
+function main(){
+    let order_lists = document.getElementById("orderProducts");
+    let orders = order_lists.querySelectorAll("tr.product-line-row");
+    for (let i = 0; i < orders.length; i++) {
+        let order = orders[i];
+        let this_tds = order.querySelectorAll("td");
+        let product = this_tds[1].querySelector("a br").innerHTML;
+        let sku = get_sku(product);
+        let qty = this_tds[3].querySelector("span").textContent;
+        sku = qty + "x" + sku;
+        log(sku);
+    }
+}
+
+log(window.setTimeout(function(){main();}, 1500));
