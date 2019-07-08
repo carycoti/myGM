e0daf2012e194e8211966c35b20fa1ea424fe1ab
Kung <sales@caryelectronic.com>
2019-07-08 23:36:14 +0800

    begin

---
+++ 淘宝订单.user.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name         淘宝订单
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  生成淘宝订单，方便写单
+// @author       Kung
+// @match        https://trade.taobao.com/trade/itemlist/list_sold_items*
+// ==/UserScript==
+
+const log = console.log.bind(console);
+
+function main() {
+    let orders = document.querySelectorAll("div.trade-order-main");
+    for (let i = 0; i < orders.length; i++) {
+        let total = "";
+        let order = orders[i];
+        let this_product = order.querySelectorAll("tr.suborder-mod__item___dY2q5");
+        for (let k = 0; k < this_product.length; k++){
+            let this_td = this_product[k].querySelectorAll("td.sol-mod__no-br___1PwLO");
+            let this_p = this_td[2].querySelector("div p");
+            let qty = this_p.innerHTML;
+            let this_span = this_td[0].querySelector("span.production-mod__sku-item___3zYoT");
+            if (this_span){
+                let this_sku = this_span.querySelectorAll("span");
+                let sku = this_sku[2].innerHTML;
+                sku = sku.replace("GZ", "");
+                let this_total = sku + "  " + qty + "个 + ";
+                total += this_total;
+            } else {
+                let this_sku = this_td[0].querySelectorAll("div div p span");
+                let sku = this_sku[6].innerHTML;
+                sku = sku.replace("GZ", "");
+                let this_total = sku + "  " + qty + "个 + ";
+                total += this_total;
+            }
+        }
+        total = total.substring(0, total.lastIndexOf(' + '));
+        log(total);
+        let my_td = document.createElement("span");
+        my_td.textContent = total;
+        my_td.className = "item-mod__create-time___1DIMJ";
+        let add_td = order.querySelector("td label");
+        add_td.parentNode.insertBefore(my_td, add_td.nextSibling);
+    }
+}
+
+log(window.setTimeout(function(){main();}, 0));
