f2ea5e02a3e464c53c47ffb36db424684d85553b
Kung <sales@caryelectronic.com>
2019-07-08 23:42:47 +0800

    2个空格

--- 淘宝订单.user.js
+++ 淘宝订单.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         淘宝订单
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  生成淘宝订单，方便写单
 // @author       Kung
 // @match        https://trade.taobao.com/trade/itemlist/list_sold_items*
@@ -24,13 +24,13 @@ function main() {
                 let this_sku = this_span.querySelectorAll("span");
                 let sku = this_sku[2].innerHTML;
                 sku = sku.replace("GZ", "");
-                let this_total = sku + "  " + qty + "个 + ";
+                let this_total = sku + "&nbsp;&nbsp;" + qty + "个 + ";
                 total += this_total;
             } else {
                 let this_sku = this_td[0].querySelectorAll("div div p span");
                 let sku = this_sku[6].innerHTML;
                 sku = sku.replace("GZ", "");
-                let this_total = sku + "  " + qty + "个 + ";
+                let this_total = sku + "&nbsp;&nbsp;" + qty + "个 + ";
                 total += this_total;
             }
         }
