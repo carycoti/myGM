c6ea0df81c4da8935acdb85b1a67b5cd94fcf659
Kung <sales@caryelectronic.com>
2020-01-03 00:46:39 +0800

    some

--- 京东夺宝岛.user.js
+++ 京东夺宝岛.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         京东夺宝岛只显示备件库自营
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.0.1
 // @description  京东夺宝岛只显示备件库自营
 // @author       Kung
 // @match        https://sell.paipai.com/auction-list/*
@@ -16,10 +16,10 @@ const log = console.log.bind(console);
 function main() {
   $(document).ready(function () {
     $("li.gl-item").each(function () {
-      let shop = $(this).find("div.gl-item");
+      let shop = $(this).find("div.p-label");
       shop = shop.text();
       GM_log(shop);
-      if ("备件库自营" == shop){
+      if (shop == "备件库自营" ){
         GM_log(shop);
       } else {
         $(this).hide();
